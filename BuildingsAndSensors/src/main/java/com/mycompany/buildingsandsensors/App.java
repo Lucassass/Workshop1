a64fed6707dd0692349e0d3fe98f803fc9e42de4
Kasper <Kasperjalris@hotmail.com>
2019-11-18 20:33:48 +0100

    Added classes
    Added classes:
    Sensor - setter and construktor
    akuator - setter and constructor
    building - constructor
    data - temp and co2 ( getters, setters)
    SuperVisionControl - arraylist with buildings

--- BuildingsAndSensors/src/main/java/com/mycompany/buildingsandsensors/App.java
+++ BuildingsAndSensors/src/main/java/com/mycompany/buildingsandsensors/App.java
@@ -13,9 +13,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + "." + " Hvordan går det?");
+        
+        var label = new Label("Hello, JavaFX ");
         var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
         stage.show();
