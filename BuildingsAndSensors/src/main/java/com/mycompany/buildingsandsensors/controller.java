6d3abc58f01c0fd82b501dd31578204683be5a89
lucas sass <lucassass@live.com>
2019-11-20 16:31:32 +0100

    Revert ":+1:"
    This reverts commit 6ed3a9e7399c24e910f16aa7da2bef55deb2c7b0.
    
    # Conflicts:
    #	BuildingsAndSensors/src/main/java/com/mycompany/buildingsandsensors/controller.java

--- BuildingsAndSensors/src/main/java/com/mycompany/buildingsandsensors/controller.java
+++ BuildingsAndSensors/src/main/java/com/mycompany/buildingsandsensors/controller.java
@@ -3,7 +3,6 @@ package src.main.java.com.mycompany.buildingsandsensors;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -13,10 +12,11 @@ public class controller implements Initializable {
 
     @FXML
     public void showAllBuildings(ActionEvent actionEvent) {
-        System.out.println(bms.getBuilding());
+        System.out.println(this.bms.getBuilding());
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.bms = new BuildingManagementSystem("1", new ArrayList<Building>());
     }
 }
